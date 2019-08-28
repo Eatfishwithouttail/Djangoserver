9fe2b0464db75d550efe63ba7969600c16176ae4
eatfishwithouttail <870815471@qq.com>
2019-08-28 21:37:11 +0800

    '完成订单表的拆分'

--- orderapp/admin.py
+++ orderapp/admin.py
@@ -1,19 +1,30 @@
 import xadmin
 from django.contrib import admin
-from orderapp.models import OrderModel
+from orderapp.models import OrderModel,OrderDetail,OrderAddress
 
 # Register your models here.
 
 
 
 class OrderAdmin(object):
-    list_display = ("num","title","price","pay_status","receiver","receiver_phone","receiver_address")
-    fields = ("num","title","price","pay_status","receiver","receiver_phone","receiver_address")
-    list_filter = ('pay_status',)
+    list_display = ("num",'user_id',"address_id",'title')
 
 
+class OrderDetailAdmmin(object):
+    list_display = ('id','num','goods_id','cnt','get_price1','get_price','pay_type','pay_status')
+    def get_price(self, obj):
+        return obj.price
+    get_price.short_description = "小计"
+    def get_price1(self, obj):
+        return obj.price1
+    get_price1.short_description = "单价"
 
+
+class OrderAddressAdmin(object):
+    list_display = ('id','user_id','receiver','receiver_phone','receiver_phone')
 
 
 
 xadmin.site.register(OrderModel,OrderAdmin)
+xadmin.site.register(OrderAddress,OrderAddressAdmin)
+xadmin.site.register(OrderDetail,OrderDetailAdmmin)
--- orderapp/models.py
+++ orderapp/models.py
@@ -3,43 +3,44 @@ from django.db import models
 # Create your models here.
 from django.db.models import Q
 
+from mainapp.models import FruitEntity, UserEntity
+
 
 class BaseModel(models.Model):
-    create_time = models.DateTimeField(verbose_name="创建时间",auto_now_add=True)
-    last_time = models.DateTimeField(verbose_name="更新时间",auto_now=True)
-
+    create_time = models.DateTimeField(verbose_name="创建时间", auto_now_add=True)
+    last_time = models.DateTimeField(verbose_name="更新时间", auto_now=True)
 
     class Meta:
-        abstract = True     # 抽象的表类
+        abstract = True  # 抽象的表类
 
 
 class OrderManager(models.Manager):
-    #获取查询结果集独享Query_set
+    # 获取查询结果集独享Query_set
     def get_queryset(self):
         return super().get_queryset().filter(~Q(pay_status=5))
 
 
-class OrderModel(BaseModel):
-    num = models.CharField(max_length=20,primary_key=True,verbose_name='订单号')
-    title = models.CharField(max_length=100,verbose_name="订单名称")
-    price = models.DecimalField(max_digits=10,decimal_places=2,verbose_name='价格')
-    pay_type = models.IntegerField(choices=((0,"余额"),
-                                            (1,"银行卡"),
-                                            (2,"微信支付"),
-                                            (3,"支付宝")),
-                                   verbose_name="支付方式",default=0)
-    pay_status = models.IntegerField(choices=((0,"待支付"),
-                                              (1,"已支付"),
-                                              (2,"待收货"),
-                                              (3,"已收货"),
-                                              (4,"完成"),
-                                              (5,"取消")),
-                                     verbose_name="订单状态",default=0)
-    receiver = models.CharField(max_length=20,null=True,verbose_name="收货人")
-    receiver_phone = models.CharField(max_length=11,null=True,verbose_name="收货人电话")
-    receiver_address = models.TextField(verbose_name="收货地址")
 
-    objects = OrderManager()
+class OrderAddress(models.Model):
+    receiver = models.CharField(max_length=20, null=True, verbose_name="收货人")
+    receiver_phone = models.CharField(max_length=11, null=True, verbose_name="收货人电话")
+    receiver_address = models.TextField(verbose_name="收货地址")
+    user_id = models.ForeignKey(UserEntity, verbose_name="用户名称", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.receiver_address
+
+    class Meta:
+        db_table = 't_address'
+        verbose_name = verbose_name_plural = '用户地址表'
+
+
+
+class OrderModel(models.Model):
+    num = models.CharField(max_length=20, primary_key=True, verbose_name='订单号')
+    user_id = models.ForeignKey(UserEntity, verbose_name="用户名称", on_delete=models.CASCADE)
+    address_id = models.ForeignKey(OrderAddress,on_delete=models.CASCADE,verbose_name='收货地址')
+    title = models.CharField(max_length=100, verbose_name="订单名称")
 
 
     def __str__(self):
@@ -48,4 +49,38 @@ class OrderModel(BaseModel):
     class Meta:
         db_table = 't_order'
         verbose_name = verbose_name_plural = '订单表'
+
+
+class OrderDetail(BaseModel):
+    num = models.ForeignKey(OrderModel, verbose_name='订单名称', on_delete=models.CASCADE)
+    goods_id = models.ForeignKey(FruitEntity, verbose_name="水果", on_delete=models.CASCADE)
+    cnt = models.IntegerField(verbose_name="数量", default=0)
+    pay_type = models.IntegerField(choices=((0, "余额"),
+                                            (1, "银行卡"),
+                                            (2, "微信支付"),
+                                            (3, "支付宝")),
+                                   verbose_name="支付方式", default=0)
+    pay_status = models.IntegerField(choices=((0, "待支付"),
+                                              (1, "已支付"),
+                                              (2, "待收货"),
+                                              (3, "已收货"),
+                                              (4, "完成"),
+                                              (5, "取消")),
+                                     verbose_name="订单状态", default=0)
+    objects = OrderManager()
+
+    def __str__(self):
+        return self.num.title
+    class Meta:
+        db_table = 't_detail'
+        verbose_name = verbose_name_plural = '订单详情表'
+
+    @property
+    def price(self):
+        return round(self.cnt*self.goods_id.price)
+
+
+    @property
+    def price1(self):
+        return self.goods_id.price
 
